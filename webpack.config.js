7b50baaca7a15e4d056edd3c37f400a4aba5d43c
trash <73361653+galenddavis@users.noreply.github.com>
2021-11-29 19:03:44 -0500

    Working to fix webpack config issues

--- webpack.config.js
+++ webpack.config.js
@@ -41,25 +41,7 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             // Just added
-            {
-                test: /\.module\.s(a|c)ss$/,
-                loader: [
-                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            sourceMap: isDevelopment
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: isDevelopment
-                        }
-                    }
-                ]
-            },
+            
             {
                 test: /\.s(a|c)ss$/,
                 exclude: /\.module.(s(a|c)ss)$/,
